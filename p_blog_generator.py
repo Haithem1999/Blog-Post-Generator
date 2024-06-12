267ca746e17764089aeb1cf1bacb3fcb1964db35
Haithem1999 <41543751+Haithem1999@users.noreply.github.com>
2024-06-12 15:35:58 +0300

    Update p_blog_generator.py

--- p_blog_generator.py
+++ p_blog_generator.py
@@ -1,5 +1,5 @@
 import json 
-from decouple import config 
+# from decouple import config 
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 from dotenv import load_dotenv
