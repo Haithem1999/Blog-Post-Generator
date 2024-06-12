982bafd6980b2b11f2fc15b10ee83080e5f3bedb
Haithem1999 <41543751+Haithem1999@users.noreply.github.com>
2024-06-12 15:23:35 +0300

    Add files via upload

---
+++ p_blog_generator.py
@@ -0,0 +1,103 @@
+import json 
+from decouple import config 
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+from dotenv import load_dotenv
+import os
+
+
+# load environment variables from .env file 
+load_dotenv()
+
+# get access to our credentials
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+MISTRAL_MODEL = os.getenv("MISTRAL_MODEL")
+
+def write_section(topic, header, sub_sections, text): 
+    
+    try:
+        SYSTEM_MESSAGE = f"You are a well trained ghostwriter for writing impressive articles about {topic}. Your articles are informative, detailed and well organized and written in a way that is easy to understand by all ages."
+        sub_section_text = ''
+        
+        for sub_section in sub_sections: 
+            sub_section_text += f"{sub_section}\n\n"
+            
+        # Mistral Chat
+        mistral_client = MistralClient(api_key = MISTRAL_API_KEY)
+        messages = [
+            
+            ChatMessage(role='system', content=SYSTEM_MESSAGE), 
+            ChatMessage(role='system', content=f"previous sections: \n{text}"), 
+            ChatMessage(role= 'user', content= f"Write section {header} with sub sections: \n {sub_section_text}"
+                                               f"Make sure to use the proper markdown formatting")
+            
+            ]
+
+        chat_response = mistral_client.chat(
+                                            model = MISTRAL_MODEL, 
+                                            messages = messages
+                                           )
+        content = chat_response.choices[0].message.content
+        return content
+        
+
+        
+    except Exception as e:
+        print(e)
+        return "", ""
+    
+    
+    
+def generate_outline(topic, subject): 
+    
+    try:
+        SYSTEM_MESSAGE = f"""You are well trained ghost writer for writing articles about {topic}.
+                            Your articles are informative, detailed, well organized 
+                            and written in a way that is easy to understand by all ages """
+        prompt = f"write an outline for article about {subject}"
+        
+        mistral_client = MistralClient(api_key=MISTRAL_API_KEY)
+        messages = [
+            ChatMessage(role='system', content=SYSTEM_MESSAGE),
+            ChatMessage(role='system', content= f"{prompt}"
+                                             "Make sure to reply with a JSON object with the following format:"
+                                             """{
+                                                "title": "Example Title",
+                                                "sections": [
+                                                    {
+                                                        "header": "Header Example",
+                                                        "sub-sections": [
+                                                            {
+                                                                "header": "Example Header"
+                                                            }
+                                                        ]
+                                                    }
+                                                ]
+                                            }"""), 
+                   ]
+        chat_response = mistral_client.chat(model = MISTRAL_MODEL, messages = messages)
+        json_response = json.loads(chat_response.choices[0].message.content)
+        return json_response
+        
+        
+    except Exception as e:
+        print(e)
+        return "",""
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ p_main.py
@@ -0,0 +1,92 @@
+import uuid 
+import streamlit as st 
+from decouple import config
+from blog_generator import write_section, generate_outline
+
+def create_blog_post(category, blog_subject): 
+    
+    try:
+        # Outline generation 
+        with st.spinner("Generating outline..."):
+            json_outline = generate_outline(topic, blog_subject)
+        blog_title = json_outline['title']
+        st.markdown(f"# {blog_title}")
+        # Write sections
+        blog_sections = json_outline['sections']
+        history_text = ''
+        for section in blog_sections: 
+            st.divider()
+            with st.spinner("Writing section {section[header]}..."):
+                content = write_section(topic, section['header'], section['sub-sections'], history_text)
+            history_text += content
+            st.markdown(content)
+        return history_text        
+        
+    except Exception as e:
+        print(e)
+        return "", ""
+    
+# page configuration
+st.set_page_config(
+    title='Blog Post Generator',
+    page_icon=':pencil2:',
+    layout = 'centered')
+
+st.title('Blog Post Generator')
+# Page description
+
+st.markdown = f"""
+Welcome to Blog Post Generator!\n\n
+This site is both simple and powerful, leveraging the latest AI advancements and unique models 
+to craft a blog post for you based on just a category and subject.\n\n
+NOTE: You can download the generated blog post as a markdown file!
+
+"""
+
+st.markdown('---')
+st.subheader('Generate Your Blog Post', anchor=False)
+category = st.selectbox('Category', options = ("", "Travel", "Technology", "Business", "Health", "Science", "Sports", "Entertainment", "Politics", "Education")
+                         
+                         )
+
+subject = st.text_input('Subject')
+generate = st.button('Generate Blog Post')
+if generate: 
+    # Validate inputs
+    error = ""
+    if category == "": 
+        error+= 'Please select a category.\n\n'
+    if subject == "": 
+        error+= 'Please select a subject.\n\n'
+    if error != "": 
+        st.error(error)
+        st.stop()
+    # Create blog post
+    st.divider()
+    history = create_blog_post(category, subject)
+    st.divider()
+    st.success("Blog Post Generated Successfully!")
+    random = uuid.uuid4().hex
+    downloaded = st.download_button("Download Blog Post", 
+                                    history, f"{category}_{subject}_{random}.md", "text/markdown", 
+                                    help="Note: Generated blog post will disappear, but you can keep the file")
+    if downloaded: 
+        # save file
+        with open(f"{category}_{subject}_{random}.md", "w") as f: 
+            f.write(history)
+            
+            
+            
+    
+    
+
+ 
+
+
+
+
+
+
+
+ 
+    
